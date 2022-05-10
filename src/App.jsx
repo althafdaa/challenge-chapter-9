3e8e0ea2c5bb2c9168ac1ee37b8d02a76f9f9e48
althafdaa <althaf.akbar@sejutacita.id>
2022-05-10 21:16:13 +0700

    Initializing repo

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ExampleComponent from './components/ExampleComponent';
+
+const App = () => {
+  return (
+    <div>
+      <ExampleComponent />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/ExampleComponent/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const ExampleComponent = () => {
+  return <div className={styles.example_component}>Hello World</div>;
+};
+
+export default ExampleComponent;
