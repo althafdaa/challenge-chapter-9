fb823e059fbc685398356fbdd8f7c204f7a342cb
althafdaa <althaf.akbar@sejutacita.id>
2022-05-11 23:01:12 +0700

    fix-folder fix folder structure and add pull request template

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import LandingPage from './pages/ExamplePage/LandingPage';
+import LandingPage from './pages/LandingPage';
 
 const App = () => {
   return (
