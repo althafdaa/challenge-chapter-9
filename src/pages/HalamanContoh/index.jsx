70c1cfb14817e8f9627afb2aecf56cee4d0eaeb4
althafdaa <100656575+althafdemiandra@users.noreply.github.com>
2022-05-13 07:42:23 +0700

    Feat: create header, main layout, and route example

---
+++ src/pages/HalamanContoh/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HalamanContoh = () => {
+  return <div>HalamanContoh</div>;
+};
+
+export default HalamanContoh;
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,10 @@
-export const ONE_HOUR = 3600000;
+export const LeftMenu = [
+  { label: 'Home', route: '/' },
+  { label: 'Game', route: '/game' },
+  { label: 'Game List', route: '/game-list' },
+];
+
+export const RightMenu = [
+  { label: 'Login', route: '/login' },
+  { label: 'Register', route: '/register' },
+];
