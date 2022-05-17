6455643985b3cf210f695bcda6c3a5599f6627d4
ndharu <96077141+ndharu@users.noreply.github.com>
2022-05-17 12:25:59 +0700

    feature-leaderboard: add halaman untuk game detail/leaderboard

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
+import GameDetail from './pages/GameDetail';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           <Route path="/login" element={<Login />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/gamedetail" element={<GameDetail />} />
         </Routes>
         <Footer />
       </BrowserRouter>
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -31,3 +31,34 @@ export const GameLists = [
     bgColor: 'bg-green-400',
   },
 ];
+
+export const TopRank = [
+  {
+    username : 'jokoneverdie',
+    score: 22230,
+    rank: 1,
+    pangkat: 'Legend',
+    profileImg : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+  },
+  {
+    username : 'joniwakwaw',
+    score: 12230,
+    rank: 2,
+    pangkat: 'Crusader',
+    profileImg : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+  },
+  {
+    username : 'bongez',
+    score: 11324,
+    rank: 3,
+    pangkat: 'Crusader',
+    profileImg : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+  },
+  {
+    username : 'mamadnakbandung',
+    score: 5200,
+    rank: 4,
+    pangkat: 'Herald',
+    profileImg : 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+  },
+];
