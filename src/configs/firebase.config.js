e24c86a27a6ff57b1eb4dc64dd41e219fe7e8f3b
althafdaa <92288572+althafdaa@users.noreply.github.com>
2022-05-17 21:23:48 +0700

    Intergrate-firebase: install firebase and connecting to repo

---
+++ src/configs/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app';
+import { getFireStore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAANZ84LNuGdXXonbwwMbiLVwvqbAM19Nc',
+  authDomain: 'game-binar.firebaseapp.com',
+  projectId: 'game-binar',
+  storageBucket: 'game-binar.appspot.com',
+  messagingSenderId: '965541150035',
+  appId: '1:965541150035:web:2cf37a672037725e6075a6',
+};
+
+initializeApp(firebaseConfig);
+
+export const db = getFireStore();
